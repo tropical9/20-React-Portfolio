b8ff111fc3425b2d31d3c49259428f8679788149
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-04-11 12:33:00 -0700

    assets, components

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import React from 'react';
+import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
-import Project from './components/Project';
+// import Project from './components/Project';
 import './App.css';
 
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <div className="container">
-            < AboutMe />
-            < Portfolio />
-            < Project />
-            < Resume />
-            < Contact />
-           
-           
-            {/* <Routes>
-              <Route path="/" element={<AboutMe />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/resume" element={<Resume />} />
-            </Routes> */}
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+	const [pages] = useState([
+		{name: "aboutme"},
+		{name: "portfolio"},
+		{name: "contact"},
+		{name: "resume"}
+	]);
+	const [currentPage, setCurrentPage] = useState("aboutme");
+
+	// This function checks to see which tab is selected and then generates the appropriate tab.
+	const selectPage = () => {
+		switch (currentPage) {
+			case "aboutme":
+				return <AboutMe />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (		
+		<div>
+			<div>
+				<Header pages={pages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+			</div>
+			<div>
+				<main>{selectPage()}</main>
+			</div>
+			<div>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
